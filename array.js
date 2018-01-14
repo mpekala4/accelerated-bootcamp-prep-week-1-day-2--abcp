484f44428380bca663c91eb333e7afaf35257adf
Mike Pekala <mpekala411@gmail.com>
2018-01-14 19:11:46 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,43 @@
 function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
  // add the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+   [array.unshift(element)];
+   return array
 }
 
 function addElementToEndOfArray(array, element) {
+  return [...array, element]
 
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  [array.push(element)]
+  return array
 }
 
 function accessElementInArray(array, index) {
+  return array[2];
 
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+function destructivelyRemoveElementFromBeginningOfArray(array,element) {
+   array.shift(element)
+   return array
 }
 
-function removeElementFromBeginningOfArray(array) {
+function removeElementFromBeginningOfArray(array,element) {
+  array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-
+function destructivelyRemoveElementFromEndOfArray(array,element) {
+  array.pop(element)
+  return array
 }
 
-function removeElementFromEndOfArray(array) {
-
+function removeElementFromEndOfArray(array,element) {
+  return array.slice(0,array.lenth-1)
 }
