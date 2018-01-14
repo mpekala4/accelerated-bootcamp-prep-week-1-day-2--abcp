5ec0af8ad4e9d32d96db870f37d57f08f706583a
Mike Pekala <mpekala411@gmail.com>
2018-01-14 17:37:33 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,18 +1,20 @@
 function addElementToBeginningOfArray(array, element) {
-  [...array,"element"];
+  ["element", ...array];
  // add the element to the beginning of the array without mutating the array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  [array.unshift("element")];
 }
 
 function addElementToEndOfArray(array, element) {
+  [...array, "element"]
 
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  [array.push("element")]
 }
 
 function accessElementInArray(array, index) {
