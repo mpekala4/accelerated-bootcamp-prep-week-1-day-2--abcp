2359aec98bebf2a23c81d05462fa7fd5c2d81e0e
Mike Pekala <mpekala411@gmail.com>
2018-01-14 18:08:16 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -25,14 +25,17 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+   array.shift
+   return array
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
