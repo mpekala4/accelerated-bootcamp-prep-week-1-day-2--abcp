b4785c8ffc9ce5d80e50528492bc7b02712e0743
Mike Pekala <mpekala411@gmail.com>
2018-01-14 18:53:47 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,9 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
-
+function greaterThanTen(num){
+if (num > 10) {
+return num
+}
+else {return "Sorry, that number is not 10!"}
+}
